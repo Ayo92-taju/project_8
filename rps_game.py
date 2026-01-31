19faa576f48a34feb2e9f8b7f46cfb9b22da5571
Ayo92-taju <69106007+Ayo92-taju@users.noreply.github.com>
2026-01-31 01:11:41 +0100

    add main function and choice prompt to rock-paper-scissors game

---
+++ rps_game.py
@@ -0,0 +1,11 @@
+import random
+
+def main():
+    name = input("\nEnter name: ")
+    
+def choice():
+    while True:
+        print("\nSelect option:\n")
+        print("1. Rock")
+        print("2. Paper")
+        print("3. Scissors")
